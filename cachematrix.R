bababbe9c790b226d9fa7f149aab27c9af422b6d
alexanderlee34 <alexanderlee34@gmail.com>
2015-06-17 18:26:31 -0400

    updated cacheMatrix R script with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function gets and saves the user-inputted matrix
+## in R's cache.
+## Then, the cacheSolve function takes the makeCacheMatrix as the input
+## to output the inverse of the matrix that the user previously inputted.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function takes a matrix as the input.  It gets the
+## matrix that the user inputted and then sets the matrix so it is saved
+## in R's cache, which is different from the current environment
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
@@ -18,7 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function takes the makeCacheMatrix function, which 
+## includes the user-inputted matrix, as the input.  It uses the local
+## functions of the makeCacheMatrix function to retrieve the matrix
+## and then calculates the inverse of the matrix using the solve()
+## function.  Afterwards, the inverse of the matrix is shown.
 
 cacheSolve <- function(x, ...) {
 	m <- x$getinverse()
